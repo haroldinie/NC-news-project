0fa77d28b9d1b4b9908b14eaa91300846733b215
haroldinie <haroldinie123@gmail.com>
2024-04-16 13:56:03 +0100

    completed challenge 4

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -1,64 +1,72 @@
 const data = require("../db/data/test-data");
-const seed = require("../db/seeds/seed")
-const db = require("../db/connection.js")
-const request = require("supertest")
-const app = require("../app.js")
-const endPoints = require('../endpoints.json')
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection.js");
+const request = require("supertest");
+const app = require("../app.js");
+const endPoints = require("../endpoints.json");
 
 beforeEach(() => {
-    return seed(data)
-})
+  return seed(data);
+});
 
 afterAll(() => {
-    db.end()
-})
+  db.end();
+});
 
 describe("GET /api/topics", () => {
-    test("responds with an array of topic objects", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then(({body}) => {
-            expect(body).toHaveLength(3)
-            body.forEach((topic) => {
-                expect(topic).toMatchObject({ description: expect.any(String), slug: expect.any(String) })
-            })
-        })
-    })
-        // .then(({body}) => {
-        //     expect(body).toHaveLength(3)
-        //     expect(body).toMatchObject([
-        //         {
-        //           description: 'The man, the Mitch, the legend',
-        //           slug: 'mitch'
-        //         },
-        //         {
-        //           description: 'Not dogs',
-        //           slug: 'cats'
-        //         },
-        //         {
-        //           description: 'what books are made of',
-        //           slug: 'paper'
-        //         }
-        //       ])
-        // })
+  test("responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toHaveLength(3);
+        body.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
 
-    describe("GET /api/topics", () => {
-        test("responds with 404 when incorrect spelling", () => {
-            return request(app)
-            .get("/api/incorrectspelling")
-            .expect(404)
-        })
-    })
+  describe("GET: 404", () => {
+    test("responds with 404 when incorrect spelling", () => {
+      return request(app).get("/api/incorrectspelling").expect(404);
+    });
+  });
 
-describe("GET /api", () => {
+  describe("GET /api", () => {
     test("responds with object describing all available endpoints", () => {
-        return request(app)
-        .get ("/api")
+      return request(app)
+        .get("/api")
         .expect(200)
-        .then(({body}) => {
-            expect(body).toEqual(endPoints)
-        })
-    })
-})
-})
+        .then(({ body }) => {
+          expect(body).toEqual(endPoints);
+        });
+    });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("responds with an article object with the correct properties", () => {
+    return request(app)
+      .get("/api/articles/2")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toMatchObject({
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          body: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+        });
+      });
+  });
+  test("GET:400, responds with status code and message when given invalid id", () => {
+    return request(app)
+    .get("/api/articles/999")
+    .expect(400)
+  })
+});
--- controller.js
+++ controller.js
@@ -1,20 +1,26 @@
-const retreiveTopics = require('./models.js');
+const { retreiveTopics, retrieveArticleById } = require("./models.js");
 
 function getTopics(req, res, next) {
-    const { query } = req
-    return retreiveTopics(query)
-
-    .then(({rows}) => {
-        res.status(200).send(rows)
+  const { query } = req;
+  return retreiveTopics(query)
+    .then(({ rows }) => {
+      res.status(200).send(rows);
     })
-    .catch((next) => {
-        
-        next(err)
-    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
+function getArticleById(req, res, next) {
+  const { article_id } = req.params; // article_id: 2
 
+  return retrieveArticleById(article_id)
+    .then((article) => {
+      return res.status(200).send(article);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
-
-
-module.exports = getTopics
+module.exports = { getTopics, getArticleById };
